233f4ec5371eb16ce982a024058c113e106d759d
rfrancotte <rodolphe.francotte@webedia-group.com>
2025-04-18 10:40:35 +0200

    format duration function moved to Utils.kt

--- app/src/main/java/com/example/contentproviderformusic/Utils.kt
+++ app/src/main/java/com/example/contentproviderformusic/Utils.kt
@@ -1,9 +1,18 @@
 package com.example.contentproviderformusic
 
 import android.media.MediaMetadataRetriever
+import java.util.Locale
+import java.util.concurrent.TimeUnit
 
 fun getImgArt(path: String): ByteArray? {
     val retriever = MediaMetadataRetriever()
     retriever.setDataSource(path)
     return retriever.embeddedPicture
+}
+
+fun formatDuration(duration: Long): String {
+    val minutes = TimeUnit.MINUTES.convert(duration, TimeUnit.MILLISECONDS)
+    val seconds = (TimeUnit.SECONDS.convert(duration, TimeUnit.MILLISECONDS) -
+            minutes * TimeUnit.SECONDS.convert(1, TimeUnit.MINUTES))
+    return String.format(Locale.FRANCE,"%02d:%02d", minutes, seconds)
 }
