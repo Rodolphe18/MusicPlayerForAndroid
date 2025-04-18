f3a8b1590446e74fdc51608040f40e4086e782d1
rfrancotte <rodolphe.francotte@webedia-group.com>
2025-04-18 14:27:14 +0200

    Exit function added to stop the service from the notification

--- app/src/main/java/com/example/contentproviderformusic/Utils.kt
+++ app/src/main/java/com/example/contentproviderformusic/Utils.kt
@@ -1,8 +1,11 @@
 package com.example.contentproviderformusic
 
+import android.app.Service
 import android.media.MediaMetadataRetriever
+import com.example.contentproviderformusic.MainActivity.Companion.musicService
 import java.util.Locale
 import java.util.concurrent.TimeUnit
+import kotlin.system.exitProcess
 
 fun getImgArt(path: String): ByteArray? {
     val retriever = MediaMetadataRetriever()
@@ -15,4 +18,14 @@ fun formatDuration(duration: Long): String {
     val seconds = (TimeUnit.SECONDS.convert(duration, TimeUnit.MILLISECONDS) -
             minutes * TimeUnit.SECONDS.convert(1, TimeUnit.MINUTES))
     return String.format(Locale.FRANCE,"%02d:%02d", minutes, seconds)
+}
+
+fun exitApplication() {
+    musicService?.let { service ->
+        service.mAudioManager?.abandonAudioFocus(service)
+        service.stopForeground(Service.STOP_FOREGROUND_REMOVE)
+        service.mMediaPlayer?.release()
+    }
+    musicService = null
+    exitProcess(1)
 }
