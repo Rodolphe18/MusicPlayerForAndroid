eaaee122c77922951b499641028e0a9538b1d2e1
rfrancotte <rodolphe.francotte@webedia-group.com>
2025-04-18 14:28:14 +0200

    MusicPlayerApplication.kt renamed MusicApplication.kt

--- app/src/main/java/com/example/contentproviderformusic/MusicApplication.kt
+++ app/src/main/java/com/example/contentproviderformusic/MusicApplication.kt
@@ -6,7 +6,7 @@ import android.app.NotificationManager
 import android.content.Context
 import android.os.Build
 
-class MusicPlayerApplication: Application() {
+class MusicApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
@@ -16,6 +16,14 @@ class MusicPlayerApplication: Application() {
             val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             notificationManager.createNotificationChannel(channel)
         }
+    }
+
+    companion object{
+        const val CHANNEL_ID = "MusicNotification"
+        const val PLAY = "play"
+        const val NEXT = "next"
+        const val PREVIOUS = "previous"
+        const val EXIT = "exit"
     }
 
 }
