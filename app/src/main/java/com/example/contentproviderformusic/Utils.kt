c7a0a9647654b8dd84329ebda37492e3c8911017
rfrancotte <rodolphe.francotte@webedia-group.com>
2025-04-19 10:23:01 +0200

    Exit function moved to service

--- app/src/main/java/com/example/contentproviderformusic/Utils.kt
+++ app/src/main/java/com/example/contentproviderformusic/Utils.kt
@@ -2,7 +2,6 @@ package com.example.contentproviderformusic
 
 import android.app.Service
 import android.media.MediaMetadataRetriever
-import com.example.contentproviderformusic.MainActivity.Companion.musicService
 import java.util.Locale
 import java.util.concurrent.TimeUnit
 import kotlin.system.exitProcess
@@ -18,14 +17,4 @@ fun formatDuration(duration: Long): String {
     val seconds = (TimeUnit.SECONDS.convert(duration, TimeUnit.MILLISECONDS) -
             minutes * TimeUnit.SECONDS.convert(1, TimeUnit.MINUTES))
     return String.format(Locale.FRANCE,"%02d:%02d", minutes, seconds)
-}
-
-fun exitApplication() {
-    musicService?.let { service ->
-        service.mAudioManager?.abandonAudioFocus(service)
-        service.stopForeground(Service.STOP_FOREGROUND_REMOVE)
-        service.mMediaPlayer?.release()
-    }
-    musicService = null
-    exitProcess(1)
 }
