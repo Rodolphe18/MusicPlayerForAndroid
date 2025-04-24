2a1dbf3ee853dee944a8bc1fa31145b28d455b9d
rfrancotte <rodolphe.francotte@webedia-group.com>
2025-04-24 13:56:44 +0200

    Enum class created in main VM to pass from main screen to song screen

--- app/src/main/java/com/example/contentproviderformusic/MainViewModel.kt
+++ app/src/main/java/com/example/contentproviderformusic/MainViewModel.kt
@@ -1,29 +1,28 @@
 package com.example.contentproviderformusic
 
-import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
 class MainViewModel : ViewModel() {
 
-    companion object {
-        private val _songs = mutableStateListOf<Song>()
-        val songs: List<Song> = _songs
-    }
-
     private val _permissionsGranted = MutableStateFlow(false)
     val permissionGranted = _permissionsGranted.asStateFlow()
 
-    fun addSong(song: Song) {
-        _songs.add(song)
-    }
+    val isPlaying = MutableStateFlow(false)
+
+    val currentSong: MutableStateFlow<Song?> = MutableStateFlow(null)
+
+    val currentDuration: MutableStateFlow<Float?> = MutableStateFlow(0f)
+
+    var screenStatus = MutableStateFlow(ScreenStatus.MAIN_SCREEN)
 
     fun updatePermissionStatus() {
         _permissionsGranted.value = true
     }
 
-    fun updateSongs(songs: List<Song>) {
-        _songs.addAll(songs)
-    }
+}
+
+enum class ScreenStatus {
+    SONG_SCREEN, MAIN_SCREEN
 }
