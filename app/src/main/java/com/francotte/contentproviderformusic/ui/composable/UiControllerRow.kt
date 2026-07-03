2dfe29839a11ff29631904191a4369895848c72f
rodolphe <rodolphefrancotte18@gmail.com>
2026-07-03 15:47:23 +0200

    UiControllerRow.kt updated

--- app/src/main/java/com/francotte/contentproviderformusic/ui/composable/UiControllerRow.kt
+++ app/src/main/java/com/francotte/contentproviderformusic/ui/composable/UiControllerRow.kt
@@ -1,18 +1,15 @@
 package com.francotte.contentproviderformusic.ui.composable
 
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
