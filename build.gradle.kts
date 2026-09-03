7ed2c16fda0af0e54064ff6a5a34f900d09a86a3
rodolphe <rodolphefrancotte18@gmail.com>
2026-09-03 17:29:35 +0200

    kotlin serialization added

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,6 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.hilt.plugin) apply false
     alias(libs.plugins.ksp) apply false
     // Firebase : appliques conditionnellement dans app/build.gradle.kts,
